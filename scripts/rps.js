12cf3c7c3d34c0e3a423719aa0adf5022680a847
Maja-Gigi <maja.hreljak@gmail.com>
2024-01-29 13:14:13 +0100

    Version addEventListener

--- scripts/rps.js
+++ scripts/rps.js
@@ -12,6 +12,30 @@ document.querySelector('.js-rock-button')
         playGame('rock');
     });
 
+document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+        playGame('paper');
+    });
+
+document.querySelector('.js-scissors-button')
+    .addEventListener('click', () => {
+        playGame('scissors');
+    });
+
+document.querySelector('.js-reset-score-button')
+    .addEventListener('click', () => {
+        score.wins = 0;
+        score.losses = 0;
+        score.ties = 0;
+        localStorage.removeItem('score');
+        updateScoreElement();
+    })
+
+document.querySelector('.js-auto-play-button')
+    .addEventListener('click', () => {
+        autoPlay();
+    });
+
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
 
